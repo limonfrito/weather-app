fa52a4a4c66735262248762110207474d54ec2cd
Salvador Garcia <chasalva7565@hotmail.com>
2024-04-16 23:37:07 -0600

    Anadida funcionalidad de locacion

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from 'expo-location'; 
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted'){
+        setError('permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })()
+  }, []);
+
+  if (location) {
+    console.log(location); 
+  }
+
   if (loading) {
     return (
       <View style={styles.container}>
         <ActivityIndicator size={'large'} color={'red'} />
       </View>
     )
-  }  
+  }
+
   return (
     <NavigationContainer>
       <Tabs iconSize={25} focusedIconColor={'tomato'} unfocusedIconColor={'black'} />
