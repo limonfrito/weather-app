b3b4f5170d8115fff784c98fa694eb84d35dd365
Salvador Garcia <chasalva7565@hotmail.com>
2024-04-17 23:53:15 -0600

    Conexion con la API del clima correcta e implementada

--- App.js
+++ App.js
@@ -2,49 +2,74 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
-import * as Location from 'expo-location'; 
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (e) {
+      setError("Could not fetch Weather");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    (async() => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted'){
-        setError('permission to access location was denied');
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("permission to access location was denied");
         return;
       }
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })()
-  }, []);
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      await fetchWeatherData();
+    })();
+  }, [lat, lon]);
 
-  if (location) {
-    console.log(location); 
+  if (weather) {
+    console.log(weather);
   }
 
   if (loading) {
     return (
       <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'red'} />
+        <ActivityIndicator size={"large"} color={"red"} />
       </View>
-    )
+    );
   }
 
   return (
     <NavigationContainer>
-      <Tabs iconSize={25} focusedIconColor={'tomato'} unfocusedIconColor={'black'} />
+      <Tabs
+        iconSize={25}
+        focusedIconColor={"tomato"}
+        unfocusedIconColor={"black"}
+      />
     </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    justifyContent: 'center',
-    flex: 1
-  }
-})
+  container: {
+    justifyContent: "center",
+    flex: 1,
+  },
+});
 
 export default App;
